a7b02a381dd3eb51fef7dd19fea2afd34cf8f99a
bisrat <bisrattegegn8@gmail.com>
2024-09-23 01:17:09 +0300

    Slight clean up

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,15 +1,5 @@
 
 import { generateRows } from "@/helpers/generator";
-// import {
-//     Table,
-//     TableBody,
-//     TableCaption,
-//     TableCell,
-//     TableHead,
-//     TableHeader,
-//     TableRow,
-//   } from "@/components/ui/table";
-// import { Button } from "./ui/button";
 
 import MarketTable from "./MarketTable";
 
@@ -19,38 +9,6 @@ export default function Home(){
     
     return (
         <div className="p-4">
-
-            {/* <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader style={{backgroundColor: "#0c1d2f"}}>
-                    <TableRow>
-                        <TableHead>Lend</TableHead>
-                        <TableHead>Points</TableHead>
-                        <TableHead>Suppy APY</TableHead>
-                        <TableHead>In Wallet</TableHead>
-                        <TableHead>Total Supply</TableHead>
-                        <TableHead>Total Borrow</TableHead>
-                        <TableHead>Utilization</TableHead>
-                        <TableHead>
-                            <Button> Edit Table </Button>
-                        </TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {
-                        rows.map((row, i) => (
-                            <TableRow key={i}>
-                                {
-                                    row.map((data, j) => <TableCell key={j}>{data}</TableCell>)
-                                }
-                                <TableCell>
-                                    <Button>Supply</Button>
-                                    <Button className="ml-2">Borrow</Button>
-                                </TableCell>
-                            </TableRow>))
-                    }
-                </TableBody>
-            </Table> */}
             <MarketTable rows={rows} headers={headers} />
         </div>
     );
