04e8934221e452c7b4b7841f75f8a3dd532edc2b
adapole <eyassubn@gmail.com>
2024-09-26 20:39:01 +0300

    add: connect wallet functionality

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,50 @@
 import { Link } from "react-router-dom";
 import Logo from "../../images/logo.png";
+import { useConnectUI, useIsConnected, useAccounts, useDisconnect } from '@fuels/react';
+import { useToast } from '@/hooks/use-toast';
 
 const Navbar = () => {
+    const { toast } = useToast();
+    const { connect, isConnecting } = useConnectUI();
+    const { isConnected } = useIsConnected();
+    const { accounts } = useAccounts();
+    const { disconnect, isPending } = useDisconnect();
+    const HandleConnect = async () => {
+        // connect();
+        
+        try {
+            await connect();
+            // console.log("Connection state", isConnected);
+            // if (isConnected) {
+                toast({
+                    title: 'Success',
+                    description: 'Wallet connected!!!',
+                    variant: 'online'
+                  });
+            // } 
+            // if (error) 
+            //     toast({
+            //         description: 'Execution Reverted',
+            //         variant: 'destructive'
+            //     })
+        } catch (error) {
+            console.warn("Error occured!")
+        }
+    }
+    const HandleDisconnect = () => {
+        toast({
+            title: 'Disconnected',
+            variant: 'online'
+        });
+        try {            
+            disconnect();
+            if (isPending) console.warn('Pending disconnect...');
+        } catch (error) {
+            console.error(error)
+        }
+            // else if (error) console.error('Error: ', error)
+    }
+    
     return (
         <div className="text-white text-xl py-2 px-5 flex justify-between mt-5" style={{backgroundColor: "rgb(8, 19, 31)", color: "rgb(221, 251, 244)"}}>
             <div>
@@ -10,7 +53,19 @@ const Navbar = () => {
                 <Link to="/leaderboard" className="mr-4">Leaderboard</Link>
             </div>
             <div>
-                <Link to="/" className="mr-4">Connect</Link>
+                {!isConnected ? (
+                    <button onClick={HandleConnect} className="mr-4">
+                        {isConnecting ? 'Connecting...' : 'Connect'}
+                    </button>
+                ):(
+                    <div className="mr-4 flex-col gap-1">
+                        <button className="mb-1 text-red-600" onClick={HandleDisconnect} >Disconnect</button>
+                        <div>
+                            {accounts ? <>{`${accounts[0]}`}</>: <>fetching</>}                        
+                        </div>
+                    </div>
+                )}
+                {/* <Link to="/" className="mr-4">Connect</Link> */}
             </div>
         </div>);
 };
