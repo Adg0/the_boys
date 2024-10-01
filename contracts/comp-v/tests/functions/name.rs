ab010fd18e55d7ee2d559fb7dbcf33e5a3dfc381
adapole <eyassubn@gmail.com>
2024-10-01 15:00:51 +0300

    fix: mint, burn and setup function for test.

--- contracts/comp-v/tests/functions/name.rs
+++ contracts/comp-v/tests/functions/name.rs
@@ -1,35 +1,62 @@
-use crate::functions::setup::{test_helpers::setup,abi_calls::{name,symbol,emit_src20_events}};
-use fuels::types::AssetId;
+use crate::functions::setup::{test_helpers::setup,abi_calls::{name,symbol,constructor,owner,set_name,set_symbol}};
+use fuels::types::{AssetId,Identity};
 use std::{string::String,str::FromStr};
 
 mod success {
     use super::*;
+    // use crate::functions::setup::abigen_bindings::compv_mod::standards::src5::State; // Import `src5::State`
 
     #[tokio::test]
-    async fn can_get_name() {
+    async fn test_name() {
         let (user, _) = setup().await;
-        let name_str = String::from_str("CompoundV");
-        emit_src20_events(&user.compv).await;
-
-        let compv_name = name(&user.compv, AssetId::default()).await.unwrap();
+        constructor(&user.compv, Identity::Address(user.wallet.address().into())).await;
         
+        // let response: State = owner(&user.compv).await;
+        
+        // // Use pattern matching to extract the Identity from the Initialized state
+        // if let State::Initialized(identity) = response {
+        //     // Check if the extracted Identity matches the expected wallet address
+        //     assert_eq!(
+        //         identity,
+        //         Identity::Address(user.wallet.address().into())
+        //     );
+        // } else {
+        //     panic!("The state is not Initialized with an Identity");
+        // } 
+
+        assert_eq!(name(&user.compv, AssetId::default()).await, None);
+
+        set_name(&user.compv, AssetId::default(), String::from("CompoundV 1")).await;
         assert_eq!(
-            name_str,
-            Ok(compv_name)
+            name(&user.compv, AssetId::default()).await,
+            Some(String::from("CompoundV 1"))
         );
     }
 
     #[tokio::test]
-    async fn can_get_symbol() {
+    async fn test_symbol() {
         let (user, _) = setup().await;
-        let symbol_str = String::from_str("COMPV");
-        emit_src20_events(&user.compv).await;
-
-        let compv_symbol = symbol(&user.compv, AssetId::default()).await.unwrap();
+        constructor(&user.compv, Identity::Address(user.wallet.address().into())).await;
         
+        // let response: State = owner(&user.compv).await;
+        
+        // // Use pattern matching to extract the Identity from the Initialized state
+        // if let State::Initialized(identity) = response {
+        //     // Check if the extracted Identity matches the expected wallet address
+        //     assert_eq!(
+        //         identity,
+        //         Identity::Address(user.wallet.address().into())
+        //     );
+        // } else {
+        //     panic!("The state is not Initialized with an Identity");
+        // } 
+
+        assert_eq!(symbol(&user.compv, AssetId::default()).await, None);
+
+        set_symbol(&user.compv, AssetId::default(), String::from("COMPV")).await;
         assert_eq!(
-            symbol_str,
-            Ok(compv_symbol)
+            symbol(&user.compv, AssetId::default()).await,
+            Some(String::from("COMPV"))
         );
     }
 }
--- contracts/comp-v/tests/functions/total_assets.rs
+++ contracts/comp-v/tests/functions/total_assets.rs
@@ -1,15 +1,41 @@
-use crate::functions::setup::{test_helpers::setup,abi_calls::{total_assets}};
+use crate::functions::setup::{test_helpers::setup,abi_calls::{total_assets,constructor,mint}};
+use fuels::types::{Bits256,Identity,Bytes32};
 
 mod success {
     use super::*;
 
     #[tokio::test]
     async fn can_get_total_assets() {
-        let (user, _) = setup().await;
+        let (user, _wallets) = setup().await;
+        constructor(&user.compv, Identity::Address(user.wallet.address().into())).await;
+        
         let assets = total_assets(&user.compv).await;
         assert_eq!(
             assets,
-            1
+            0
         );
+
     }
+
+    #[tokio::test]
+    async fn test_change_total_assets() {
+        let (user, _wallets) = setup().await;
+        constructor(&user.compv, Identity::Address(user.wallet.address().into())).await;
+        
+        let assets = total_assets(&user.compv).await;
+        assert_eq!(
+            assets,
+            0
+        );
+
+        let recipient = Identity::Address(_wallets[1].address().into());
+        let sub_id_1 = Bytes32::from([1u8; 32]);
+    
+        mint(&user.compv, recipient, Bits256(*sub_id_1), 100).await;
+        
+        let assets = total_assets(&user.compv).await;
+        // println!("Assets: {assets}");
+        assert_eq!(assets, 1);
+    }
+
 }
