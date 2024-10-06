b18a3ccc87d697a2e591558bc767979431d14833
Adg0 <kalebbirru@gmail.com>
2024-10-06 22:36:31 +0300

    feeConverter with contract: completed

--- services/feeConverter/tests/harness.rs
+++ services/feeConverter/tests/harness.rs
@@ -6,7 +6,7 @@ abigen!(Contract(
     abi = "out/debug/feeConverter-abi.json"
 ));
 
-async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
+async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId, WalletUnlocked) {
     // Launch a local network and deploy the contract
     let mut wallets = launch_custom_provider_and_get_wallets(
         WalletsConfig::new(
@@ -30,14 +30,52 @@ async fn get_contract_instance() -> (MyContract<WalletUnlocked>, ContractId) {
     .await
     .unwrap();
 
-    let instance = MyContract::new(id.clone(), wallet);
+    let instance = MyContract::new(id.clone(), wallet.clone());
 
-    (instance, id.into())
+    let (_tx_id, _receipts) = wallet
+            .force_transfer_to_contract(
+                &id.clone(),
+                1_000_000,
+                AssetId::zeroed(),
+                TxPolicies::default(),
+            )
+            .await.unwrap();
+    let contract_balances = wallet
+            .try_provider().unwrap()
+            .get_contract_balances(&id.clone())
+            .await.unwrap();
+    println!("Starting Contract balance = {:?}", contract_balances);
+    let balance: u64 = wallet.get_asset_balance(&AssetId::zeroed()).await.unwrap();
+    println!("Starting wallet Balance: {:?}", balance);
+    (instance, id.into(), wallet)
 }
 
 #[tokio::test]
 async fn can_get_contract_id() {
-    let (_instance, _id) = get_contract_instance().await;
+    let (instance, _id, wallet) = get_contract_instance().await;
 
     // Now you have an instance of your contract you can use to test each function
+    let call_params = CallParameters::default()
+        .with_amount(1_000_000)
+        .with_asset_id(AssetId::zeroed());
+    let mut asset_ids: Vec<AssetId> = Vec::new();
+    asset_ids.push(AssetId::zeroed());
+
+    let auction = instance.methods()
+        .buy(asset_ids.clone())
+        .with_variable_output_policy(VariableOutputPolicy::Exactly(1))
+        .call_params(call_params.clone()).unwrap().call().await;
+    let contract_balances =wallet
+        .try_provider().unwrap()
+        .get_contract_balances(&_id.clone().into())
+        .await.unwrap();
+    // assert!(contract_balances.is_empty());
+
+    let log = auction.expect("REASON").decode_logs_with_type::<Slot>();
+    println!("Logs: {:?}", log);
+
+    // println!("Return Auction: {:?}", auction);
+    println!("Contract Balance: {:?}", contract_balances);
+    let balance: u64 = wallet.get_asset_balance(&AssetId::zeroed()).await.unwrap();
+    println!("Wallet Balance: {:?}", balance);
 }
